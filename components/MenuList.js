5bccb4a5e5baa7fd69049d25edbc20427506eef1
sebaszwenger <sebaszwenger@gmail.com>
2022-07-06 01:47:35 -0300

    se modificaron las props. se agrego un un filtro al arreglo carta antes de enviarlo al componente MenuCard

--- components/MenuList.js
+++ components/MenuList.js
@@ -1,21 +1,15 @@
 import MenuCard from "./MenuCard";
 
-const MenuList = ({ menuList }) => {
+const MenuList = ({ carta, tipoMenu }) => {
   return (
     <ul className="max-w-[85%] mx-auto text-decoration-none grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 ">
-      {menuList?.map((item, i) => (
-        <MenuCard item={item} key={i} />
-      ))}
+      {tipoMenu === "Todos"
+        ? carta?.map((item, i) => <MenuCard item={item} key={i} />)
+        : carta
+            .filter((item) => item.nombre.includes(tipoMenu))
+            .map((item, i) => <MenuCard item={item} key={i} />)}
     </ul>
   );
 };
 
 export default MenuList;
-
-{
-  /* <ul className="max-w-[85%] lg:px-8 mx-auto w-full h-full xl:px-12 2xl:px-16 flex flex-wrap text-decoration-none md:gap-x-2 lg:gap-x-6 2xl:gap-x-10">
-  {menuList?.map((item, i) => (
-    <MenuCard item={item} key={i} />
-  ))}
-</ul>; */
-}
