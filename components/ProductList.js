e2f30aef38e1007180eec0b2bc9aa9afc2f4da2a
sebaszwenger <sebaszwenger@gmail.com>
2022-07-06 01:28:33 -0300

    se importo useState. se creo el state tipoMenu. se cambio la etiqueta p x h2 y se cambio el encabezado. se pasaron las props de seTipoMenu y tipoMenu al componente TipoMilaBar. se paso la prop de tipoMenu al componente MenuList.

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,14 +1,17 @@
+import React, { useState } from "react";
+
 import TipoMilaBar from "./TipoMilaBar";
 import MenuList from "./MenuList";
 
 const ProductList = ({ carta }) => {
+  const [tipoMenu, setTipoMenu] = useState("todas");
   return (
     <div className="w-full">
-      <p className="text-center py-8 text-2xl md:text-3xl xl:text-4xl font-semibold tracking-wide ">
-        Nuestros Productos
-      </p>
-      <TipoMilaBar />
-      <MenuList menuList={carta} />
+      <h2 className="text-center py-8 text-2xl md:text-3xl xl:text-4xl font-semibold tracking-wide uppercase">
+        El Menú más Groso que existe!
+      </h2>
+      <TipoMilaBar setTipoMenu={setTipoMenu} tipoMenu={tipoMenu} />
+      <MenuList carta={carta} tipoMenu={tipoMenu} />
     </div>
   );
 };
