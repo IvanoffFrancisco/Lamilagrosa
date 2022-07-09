ca6714dbb32df0015c5bbad5feae55ae06f7c2b0
Sebastián Zwenger <sebaszwenger@gmail.com>
2022-07-09 02:29:14 -0300

    se mejoro el responsive design

--- components/ProductList.js
+++ components/ProductList.js
@@ -7,8 +7,8 @@ const ProductList = ({ carta }) => {
   const [tipoMenu, setTipoMenu] = useState("Todos");
   return (
     <div className="w-full">
-      <h2 className="text-center py-8 text-2xl md:text-3xl xl:text-4xl font-semibold tracking-wide uppercase">
-        El Menú más Groso que existe!
+      <h2 className="text-center py-8 text-lg md:text-2xl xl:text-3xl font-semibold md:tracking-wide 2xl:tracking-widest uppercase">
+        El Menú más <span className="text-blue-500">Groso</span> que existe!
       </h2>
       <TipoMilaBar setTipoMenu={setTipoMenu} tipoMenu={tipoMenu} />
       <MenuList carta={carta} tipoMenu={tipoMenu} />
