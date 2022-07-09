5423e858d9ddf0e3c36dc642a1561afd51465ded
Sebastián Zwenger <sebaszwenger@gmail.com>
2022-07-09 02:29:46 -0300

    se mejoro el responsive design

--- components/MenuList.js
+++ components/MenuList.js
@@ -2,7 +2,7 @@ import MenuCard from "./MenuCard";
 
 const MenuList = ({ carta, tipoMenu }) => {
   return (
-    <ul className="max-w-[85%] mx-auto text-decoration-none grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 ">
+    <ul className="max-w-[85%] mx-auto text-decoration-none grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5 md:pag-4">
       {tipoMenu === "Todos"
         ? carta?.map((item, i) => <MenuCard item={item} key={i} />)
         : carta
