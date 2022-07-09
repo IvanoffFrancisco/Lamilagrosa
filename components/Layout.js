d9da332fef28e84d84a62ecaa3e7c84a2301596e
sebaszwenger <sebaszwenger@gmail.com>
2022-07-09 19:44:37 -0300

    se modifico el Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,21 @@
+import Head from "next/head";
+
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, pagina }) => {
   return (
-    <>
+    <div>
+      <Head>
+        <title>La Mila Grosa - </title>
+        <meta name="description" content="Sitio Web de venta de comida" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Navbar />
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from "next/head";
 import Articulo1 from "../components/Articulo1";
 import Carrusel from "../components/Carrusel";
+import Layout from "../components/Layout";
 
 const images = [
   "/img/slideshow/sandwich-de-milanesa.jpg",
@@ -10,21 +10,16 @@ const images = [
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>La Mila Grosa</title>
-        <meta name="description" content="La mejor milanesa" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <Layout>
       <Carrusel
         controles={true}
-        autoplay={false}
+        autoplay={true}
         velocidad="700"
         intervalo="4500"
         images={images}
       />
+
       <Articulo1 />
-    </div>
+    </Layout>
   );
 }
