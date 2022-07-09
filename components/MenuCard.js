24f3c582261a1a997651828a5fa0616493e5ccbb
Sebastián Zwenger <sebaszwenger@gmail.com>
2022-07-09 02:35:08 -0300

    se modificaron estilos. se agrego un boton a la card que dirige al usuario a la pagina del producto.

--- components/MenuCard.js
+++ components/MenuCard.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const MenuCard = ({ item }) => {
   return (
-    <li className="text-center bg-black shadow-md shadow-gray-700 cursor-pointer hover:ring-4 hover:ring-blue-600">
+    <li className="h-full text-center bg-black shadow-md shadow-gray-700 hover:ring-4 hover:ring-red-600 rounded-md">
       <div>
         <Image
           src={item.imagen}
@@ -11,11 +11,17 @@ const MenuCard = ({ item }) => {
           height="400"
           alt="producto"
           objectFit="cover"
+          className="rounded-md"
         />
-        <h2 className="font-bold text-lg text-white py-2 lg:text-lg xl:text-xl">
-          {item.nombre}
-        </h2>
-        <p className="text-blue-600 text-lg pb-2">$ {item.precio}</p>
+        <div>
+          <h2 className="font-bold text-lg text-white pt-2 px-1">
+            {item.nombre}
+          </h2>
+          <div className="flex justify-evenly items-center py-3">
+            <p className="text-red-600 text-2xl pb-2">$ {item.precio}</p>
+            <button className="py-1 px-3 bg-red-600 text-white text-lg rounded-md">Añadir</button>
+          </div>
+        </div>
       </div>
     </li>
   );
