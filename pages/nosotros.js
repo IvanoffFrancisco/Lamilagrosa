4abec625fd5224e244ab29081b7efc75aff6ef0a
sebaszwenger <sebaszwenger@gmail.com>
2022-07-09 19:58:20 -0300

    se envolvio a las pages con el Layout

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,49 +1,50 @@
 import Image from "next/image";
+import Layout from "../components/Layout";
 
 const nosotros = () => {
   return (
-    <div className="w-full h-auto mt-20">
-      <div className="max-w-[95%] md:max-w-[90%] lg:max-w-[85%] mx-auto">
+    <Layout>
+      <div className="w-full h-auto mt-20">
+        <div className="max-w-[95%] md:max-w-[90%] lg:max-w-[85%] mx-auto">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center py-8 lg:py-10 xl:py-12 2xl:py-14 tracking-widest">
+            Nuestra Historia
+          </h2>
 
-        <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center py-8 lg:py-10 xl:py-12 2xl:py-14 tracking-widest">
-          Nuestra Historia
-        </h2>
+          <div className="w-full flex flex-col md:flex-row md:text-lg lg:text-lg xl:text-2xl 2xl:text-3xl gap-3">
+            <div className="w-full md:min-w-[400x]">
+              <Image
+                src="/img/nosotros.jpg"
+                alt="nosotros"
+                layout="responsive"
+                width="600"
+                height="800"
+                objectFit="cover"
+              />
+            </div>
 
-        <div className="w-full flex flex-col md:flex-row md:text-lg lg:text-lg xl:text-2xl 2xl:text-3xl gap-3">
-
-          <div className="w-full md:min-w-[400x]">
-            <Image
-              src="/img/nosotros.jpg"
-              alt="nosotros"
-              layout="responsive"
-              width="600"
-              height="800"
-              objectFit="cover"
-            />
-          </div>
-
-          <div className="w-full leading-6 lg:leading-8 xl:leading-10">
-            <p>La Mila Grosa abrió sus puertas en Enero del 2011.</p>
-            <p>
-              Fieles a la fusión de los mejores ingredientes locales e
-              internacionales nos mantenemos firmes con nuestra premisa,
-              ofrecerte calidad, frescura y confianza. Atentos al crecimiento de
-              paladares exigentes que requieren constantes avances en propuestas
-              gastronómicas, innovados en nuestro menú, siguiendo las últimas
-              tendencias nutricionales. Así siempre llegaran a tu mesa productos
-              sumamente confiables y de inmejorable calidad.
-            </p>
-            <p>
-              Posicionados actualmente como referente en el rubro, La Mila Grosa
-              es un emprendimiento gastronomico en plena expansión que se
-              consolida día a día y por eso hoy, una vez más, queremos darte las
-              gracias por acompañarnos, por elegirnos y confiar en nuestra
-              propuesta y sobre todo en nosotros.
-            </p>
+            <div className="w-full leading-6 lg:leading-8 xl:leading-10">
+              <p>La Mila Grosa abrió sus puertas en Enero del 2011.</p>
+              <p>
+                Fieles a la fusión de los mejores ingredientes locales e
+                internacionales nos mantenemos firmes con nuestra premisa,
+                ofrecerte calidad, frescura y confianza. Atentos al crecimiento
+                de paladares exigentes que requieren constantes avances en
+                propuestas gastronómicas, innovados en nuestro menú, siguiendo
+                las últimas tendencias nutricionales. Así siempre llegaran a tu
+                mesa productos sumamente confiables y de inmejorable calidad.
+              </p>
+              <p>
+                Posicionados actualmente como referente en el rubro, La Mila
+                Grosa es un emprendimiento gastronomico en plena expansión que
+                se consolida día a día y por eso hoy, una vez más, queremos
+                darte las gracias por acompañarnos, por elegirnos y confiar en
+                nuestra propuesta y sobre todo en nosotros.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
