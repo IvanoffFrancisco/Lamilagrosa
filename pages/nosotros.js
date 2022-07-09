3752c2f738db48671a05b131ebc064526de0aebf
Sebastián Zwenger <sebaszwenger@gmail.com>
2022-07-09 01:32:05 -0300

    se cambio la historia y se agrego responsive.

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -2,42 +2,45 @@ import Image from "next/image";
 
 const nosotros = () => {
   return (
-    <div className="w-full h-screen ">
-      <h2 className="text-2xl font-bold my-10 text-center">
-        Acerca de nosotros
-      </h2>
-      <div className="max-w-[80%] flex justify-center gap-x-2 mx-auto ">
-        <div className="w-1/3 h-auto">
-          <Image
-            src="/img/nosotros.jpg"
-            alt="cheff"
-            layout="responsive"
-            width="600"
-            height="800"
-            // objectFit="cover"
-          />
-        </div>
-        <div className="max-w-[40%]">
-          <p>
-            En la Mila Grosa buscamos satisfacer las necesidades gastronómicas
-            de los clientes, ofreciendo alimentos y servicios con la más alta
-            calidad, donde se sobrepasen las expectativas de los clientes
-            pudiendo hacer, que siempre deseen volver a confiar en nuestros
-            productos.
-          </p>
-          <p>
-            Tomando como estrategia principal el mejoramiento continúo de los
-            estrictos estándares de higiene y buen servicio a un precio justo.
-          </p>
-          <p>
-            Nuestra vision es ser reconocidos en el ámbito nacional, como un
-            grupo de trabajo original, sólido y profesional, con calidad humana
-            y principios éticos, que ofrece servicios y productos de excelencia
-            a sus clientes, posibilidades de desarrollo a su personal, trato
-            justo a sus proveedores; transparencia y buenos manejos a las
-            autoridades y colaboración en el desarrollo de la atracción
-            turística en nuestra región
-          </p>
+    <div className="w-full h-auto mt-20">
+      <div className="max-w-[95%] md:max-w-[90%] lg:max-w-[85%] mx-auto">
+
+        <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center py-8 lg:py-10 xl:py-12 2xl:py-14 tracking-widest">
+          Nuestra Historia
+        </h2>
+
+        <div className="w-full flex flex-col md:flex-row md:text-lg lg:text-lg xl:text-2xl 2xl:text-3xl gap-3">
+
+          <div className="w-full md:min-w-[400x]">
+            <Image
+              src="/img/nosotros.jpg"
+              alt="nosotros"
+              layout="responsive"
+              width="600"
+              height="800"
+              objectFit="cover"
+            />
+          </div>
+
+          <div className="w-full leading-6 lg:leading-8 xl:leading-10">
+            <p>La Mila Grosa abrió sus puertas en Enero del 2011.</p>
+            <p>
+              Fieles a la fusión de los mejores ingredientes locales e
+              internacionales nos mantenemos firmes con nuestra premisa,
+              ofrecerte calidad, frescura y confianza. Atentos al crecimiento de
+              paladares exigentes que requieren constantes avances en propuestas
+              gastronómicas, innovados en nuestro menú, siguiendo las últimas
+              tendencias nutricionales. Así siempre llegaran a tu mesa productos
+              sumamente confiables y de inmejorable calidad.
+            </p>
+            <p>
+              Posicionados actualmente como referente en el rubro, La Mila Grosa
+              es un emprendimiento gastronomico en plena expansión que se
+              consolida día a día y por eso hoy, una vez más, queremos darte las
+              gracias por acompañarnos, por elegirnos y confiar en nuestra
+              propuesta y sobre todo en nosotros.
+            </p>
+          </div>
         </div>
       </div>
     </div>
