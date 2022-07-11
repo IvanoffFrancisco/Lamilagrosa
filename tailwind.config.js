26529f6da5e3653c8461c0a1adb3c1db15857238
sebaszwenger <sebaszwenger@gmail.com>
2022-07-11 08:43:45 -0300

    se eliminaron clases personalizadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require("tailwindcss/plugin");
 
-const Myclass = plugin(function ({ addUtilities }) {
-  addUtilities({
-    ".my-rotate-y-180": {
-      transform: "rotateY(180deg)",
-    },
-    ".preserve-3d": {
-      transformStyle: "preserve-3d",
-    },
-    ".perspective": {
-      perspective: "1000px",
-    },
-    ".backface-hidden": {
-      backfaceVisibility: "hidden",
-    },
-  });
-});
-
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        seba: "repeat(auto-fit, minmax(30rem,1fr))",
-      },
-    },
+    extend: {},
   },
-  plugins: [Myclass],
+  plugins: [],
 };
