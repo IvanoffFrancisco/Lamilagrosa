ebb7aa58c34845e165cc1d539c55f032e7fabe55
sebaszwenger <sebaszwenger@gmail.com>
2022-07-13 19:44:32 -0300

    se hicieron cambio de estilos y se agrego la logica para almacenar y validar los campos del form

--- pages/register.js
+++ pages/register.js
@@ -11,13 +11,73 @@ import {
   FaUser,
 } from "react-icons/fa";
 import { MdLockOutline, MdLock, MdAddLocationAlt } from "react-icons/md";
+import { useState } from "react";
+import Error from "../components/Error";
 
-export default function register() {
+export default function Register() {
+  //stores the form fields
+  const [user, setUser] = useState({
+    nombreUsuario: "",
+    email: "",
+    direccion: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  //state that checks if there is an error in the form data
+  const [error, setError] = useState(false);
+  //state that saves the error message to print on the screen
+  const [message, setMessage] = useState("");
+
+  //extract values ​​from user state
+  const { nombreUsuario, email, direccion, password, passwordConfirm } = user;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (password === passwordConfirm) {
+      // console.log(
+      //   `Los datos del registro son:
+      //   Nombre de usuario: ${nombreUsuario}
+      //   Direccion: ${direccion}
+      //   Email ${email}
+      //   Password ${password}`
+      // );
+
+      // Reset the form
+      setUser({
+        nombreUsuario: "",
+        email: "",
+        direccion: "",
+        password: "",
+        passwordConfirm: "",
+      });
+
+      setError(false);
+    } else {
+      setError(true);
+      setMessage("Las contraseñas no coinciden");
+    }
+  }
+
+  //stores the data entered in each field of the form
+  function onChange(e) {
+    setUser({
+      ...user,
+      [e.target.id]: e.target.value,
+    });
+  }
+
   return (
     <div className="w-full h-screen bg-gray-200 flex items-center">
       <main className="w-[850px] mx-auto flex flex-col-reverse md:flex-row-reverse justify-center text-center max-w-[85%]">
-        <div className="w-full md:w-1/2 max-w-[425px] mx-auto bg-white py-10 flex justify-center items-center rounded-l-lg ">
-          <form action="#" method="GET" className="w-full px-9 py-1">
+        <div className="w-full md:w-1/2 max-w-[425px] mx-auto bg-white py-10 flex justify-center items-center rounded-r-lg ">
+          <form
+            action="#"
+            method="GET"
+            className="w-full px-9 py-1"
+            onSubmit={handleSubmit}
+          >
             <Image
               src={LoginImage}
               alt="Picture of the author"
@@ -27,62 +87,73 @@ export default function register() {
             <h2 className="text-3xl font-semibold text-sky-500 pt-3 pb-5 ">
               Crea tu Cuenta
             </h2>
-
+            {error && <Error message={message} />}
             <div className="w-full flex flex-col items-center">
-              <div className="w-full my-1 bg-gray-100 flex items-center rounded-sm">
+              <div className="w-full my-1 bg-gray-100 flex items-center rounded-md px-1">
                 <FaUser className="text-sky-400 mx-2" />
                 <input
                   type="text"
-                  name="name"
-                  placeholder="Usuario"
+                  value={nombreUsuario}
+                  id="nombreUsuario"
+                  placeholder="Nombre de usuario"
                   className="bg-gray-100 outline-none text-sm py-3 w-full"
                   required
+                  onChange={onChange}
                 />
               </div>
-              <div className="w-full my-1 bg-gray-100 flex items-center rounded-sm">
+              <div className="w-full my-1 bg-gray-100 flex items-center rounded-md px-1">
                 <FaRegEnvelope className="text-sky-400 mx-2" />
                 <input
                   type="email"
-                  name="email"
+                  value={email}
+                  id="email"
                   placeholder="Email"
                   className="bg-gray-100 outline-none text-sm py-3 w-full"
                   required
+                  onChange={onChange}
                 />
               </div>
-              <div className="w-full my-1 bg-gray-100 flex items-center rounded-sm">
-                <MdLockOutline className="text-sky-400 mx-2" />
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Contraseña"
-                  className="bg-gray-100 outline-none text-sm py-3 w-full"
-                  required
-                />
-              </div>
-              <div className="w-full my-1 bg-gray-100 flex items-center rounded-sm">
-                <MdLock className="text-sky-400 mx-2" />
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Confirmar contraseña"
-                  className="bg-gray-100 outline-none text-sm py-3 w-full"
-                  required
-                />
-              </div>
-              <div className="w-full my-1 bg-gray-100 flex items-center rounded-sm">
+              <div className="w-full my-1 bg-gray-100 flex items-center rounded-md px-1">
                 <MdAddLocationAlt className="text-sky-400 mx-2" />
                 <input
                   type="text"
-                  name="direccion"
-                  placeholder="Agregar una dirección"
+                  value={direccion}
+                  id="direccion"
+                  placeholder="Dirección postal"
                   className="bg-gray-100 outline-none text-sm py-3 w-full"
                   required
+                  onChange={onChange}
                 />
               </div>
+              <div className="w-full my-1 bg-gray-100 flex items-center rounded-md px-1">
+                <MdLockOutline className="text-sky-400 mx-2" />
+                <input
+                  type="password"
+                  value={password}
+                  id="password"
+                  placeholder="Contraseña"
+                  className="bg-gray-100 outline-none text-sm py-3 w-full"
+                  required
+                  onChange={onChange}
+                />
+              </div>
+              <div className="w-full my-1 bg-gray-100 flex items-center rounded-md px-1">
+                <MdLock className="text-sky-400 mx-2" />
+                <input
+                  type="password"
+                  value={passwordConfirm}
+                  id="passwordConfirm"
+                  placeholder="Confirmar contraseña"
+                  className="bg-gray-100 outline-none text-sm py-3 w-full"
+                  required
+                  onChange={onChange}
+                />
+              </div>
+
               <button className="px-10 py-2  mt-7 text-sky-400 font-bold border-2 border-sky-400 rounded-full hover:bg-sky-500 ease-in duration-300 hover:text-white tracking-wide uppercase">
                 Registrarse
               </button>
-              <label className="flex items-center text-[11px] mt-1 mb-6">
+              <label className="flex items-center text-[11px] mt-1 mb-6 tracking-wide">
                 <input type="checkbox" name="remember" className="mr-1" />
                 Acepta someter su paladar a una nueva experiencia?
               </label>
@@ -110,29 +181,26 @@ export default function register() {
           </form>
         </div>
 
-        <div className="hidden md:w-1/2 bg-sky-500 text-white md:flex justify-center items-center rounded-r-lg px-9">
+        <div className="hidden md:w-1/2 bg-sky-500 text-white md:flex justify-center items-center rounded-l-lg px-9">
           <div>
             <h2 className="text-3xl font-semibold pb-3 tracking-wide">
               Bienvenido!
             </h2>
-            <p>
+            <p className="mb-2 tracking-wide">
               Estas a punto de ingresar a nuestra gran familia de
               <span className="text-red-600 font-semibold"> Mila</span>
               <span className="text-black font-semibold">Grosos.</span>
             </p>
-            <p>
+            <p className="tracking-wide">
               Al registrarte aceptas someter tu paladar a una nueva experiencia
               de la que nunca querras volver
-              {/* <span className="text-black font-semibold">La </span>
-              <span className="text-red-600 font-semibold">Mila </span>
-              <span className="text-black font-semibold">Grosa</span> */}
             </p>
 
-            <p className="mt-16 text-xs text-gray-800">
+            <p className="mt-16 text-xs text-gray-800 tracking-wide">
               Si ya tenes una cuenta hace click aquí
             </p>
             <Link href="/login">
-              <button className="border-2 border-white rounded-full mt-2 px-12 py-2 inline-block font-semibold hover:bg-white hover:text-sky-500 ease-in duration-300 uppercase">
+              <button className="border-2 border-white rounded-full mt-2 px-12 py-2 inline-block font-semibold hover:bg-white hover:text-sky-500 ease-in duration-300 uppercase tracking-wide">
                 Iniciar Sesión
               </button>
             </Link>
