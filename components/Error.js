05c23bea821cda33ef962e8d5625d57389ee3275
Sebastián Zwenger <sebaszwenger@gmail.com>
2022-07-13 01:24:36 -0300

    se creo el componente Error que muestra un mensaje de error en pantalla

---
+++ components/Error.js
@@ -0,0 +1,9 @@
+function Error({ message, setError, error }) {
+  return (
+    <p className="bg-red-600 w-full p-2 text-white uppercase font-semibold rounded-md text-center my-1 text-xs">
+      {message}
+    </p>
+  );
+}
+
+export default Error;
