d1a0a5fb99d8f9797bd5aca385226140381b08aa
sebaszwenger <sebaszwenger@gmail.com>
2022-07-26 19:41:32 -0300

    se paso el id de producto con key del map

--- components/MenuList.js
+++ components/MenuList.js
@@ -4,10 +4,10 @@ const MenuList = ({ carta, tipoMenu }) => {
   return (
     <ul className="max-w-[85%] mx-auto text-decoration-none grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5 md:pag-4">
       {tipoMenu === "Todos"
-        ? carta?.map((item, i) => <MenuCard item={item} key={i} />)
+        ? carta?.map((item) => <MenuCard item={item} key={item._id} />)
         : carta
             .filter((item) => item.nombre.includes(tipoMenu))
-            .map((item, i) => <MenuCard item={item} key={i} />)}
+            .map((item, i) => <MenuCard item={item} key={item._id} />)}
     </ul>
   );
 };
