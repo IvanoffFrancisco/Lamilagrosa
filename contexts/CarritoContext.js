04292ff6879dea02a8e807e38d6629326c39b32a
Sebastián Zwenger <sebaszwenger@gmail.com>
2022-07-26 02:45:09 -0300

    se agrego un nuevo stado al context de carrito.

--- contexts/CarritoContext.js
+++ contexts/CarritoContext.js
@@ -4,9 +4,10 @@ export const CarritoContext = createContext();
 
 export const CarritoProvider = ({ children }) => {
   const [carrito, setCarrito] = useState([]);
+  const [editarProducto, setEditarProducto] = useState("");
 
   return (
-    <CarritoContext.Provider value={[ carrito, setCarrito ]}>
+    <CarritoContext.Provider value={[ carrito, setCarrito, editarProducto, setEditarProducto]}>
       {children}
     </CarritoContext.Provider>
   );
