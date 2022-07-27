b55c7a41acf455a5d68c00fbff611b6be48c0cc0
sebaszwenger <sebaszwenger@gmail.com>
2022-07-26 21:35:07 -0300

    nada

--- contexts/UsuarioContext.js
+++ contexts/UsuarioContext.js
@@ -1,18 +1,16 @@
-import {createContext,useState} from 'react';
+import { createContext, useState } from "react";
 
-export const UsuarioContext=createContext();
+export const UsuarioContext = createContext();
 
+export const UsuarioProvider = ({ children }) => {
+  const [userGlobal, setUserGlobal] = useState({});
+  const [isloged, setIsloged] = useState(false);
 
-export const UsuarioProvider=({children})=>{
-    const [userGlobal, setUserGlobla] = useState({
-        
-    });
-
-    const [isloged, setIsloged] = useState(false);
-
-    return(
-        <UsuarioContext.Provider value={{isloged, setIsloged,userGlobal, setUserGlobla}}>
-            {children}
-        </UsuarioContext.Provider>
-    )
-}
+  return (
+    <UsuarioContext.Provider
+      value={{ isloged, setIsloged, userGlobal, setUserGlobal }}
+    >
+      {children}
+    </UsuarioContext.Provider>
+  );
+};
