260b43771698763acef6167b327bae00d52ed33e
Sebastián Zwenger <sebaszwenger@gmail.com>
2022-07-28 03:20:31 -0300

    se elimino el manejo del carrito y se paso a CarritoContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,51 +1,18 @@
 import "../styles/globals.css";
 import { MenuProvider } from "../contexts/MenuContext";
 import { UsuarioProvider } from "../contexts/UsuarioContext";
-import { useEffect, useState } from "react";
-// import {CarritoProvider } from "../contexts/CarritoContext";
+import { CarritoProvider } from "../contexts/CarritoContext";
 
 function MyApp({ Component, pageProps }) {
-  const [carrito, setCarrito] = useState([]);
-  // const [isLogged, setIsLogged] = useState(false);
-
-  useEffect(() => {
-    const carritoLS = JSON.parse(localStorage.getItem("LMG-Carrito")) ?? [];
-    console.log(carritoLS);
-    setCarrito(carritoLS);
-
-    // setIsLogged(localStorage.getItem("isLogged"));
-
-    // setUserGlobal(localStorage.getItem("user"));
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("LMG-Carrito", JSON.stringify(carrito));
-  }, [carrito]);
-
-  const agregarCarrito = (pedido) => {
-    setCarrito([...carrito, pedido]);
-  };
-
-  const eliminarCarrito = (id) => {
-    const respuesta = confirm("Deseas eliminar este producto?");
-    if (respuesta) {
-      const carritoActualizado = carrito?.filter(
-        (elemento) => elemento.id !== id
-      );
-      setCarrito(carritoActualizado);
-    }
-  };
 
   return (
     <MenuProvider>
       <UsuarioProvider>
+      <CarritoProvider>
         <Component
           {...pageProps}
-          carrito={carrito}
-          setCarrito={setCarrito}
-          agregarCarrito={agregarCarrito}
-          eliminarCarrito={eliminarCarrito}
         />
+        </CarritoProvider>
       </UsuarioProvider>
     </MenuProvider>
   );
