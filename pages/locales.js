23edca040edebc7fbc0ddaedb35265982982c984
Martin Bosch <92443557+MbochaL@users.noreply.github.com>
2022-07-30 13:57:33 -0300

    Update locales.js

--- pages/locales.js
+++ pages/locales.js
@@ -395,19 +395,3 @@ export default function infoCards() {
   );
 }
 
-
-{/* <span className="text-gray-700 font-bold pr-2">Dirección:</span>{" "}
-              Carlos Pellegrini 1252, W3400BAW, Carlos Pellegrini, W3400BAW
-              Corrientes
-              <a href="https://www.google.com/maps/place/Carlos+Pellegrini+1239,+W3400BAU+Corrientes/@-27.4655888,-58.8368812,17z/data=!4m12!1m6!3m5!1s0x94456ca435a86d4b:0xb5eee1704ac6160e!2sMaruja+Resto+%26+Bar!8m2!3d-27.465584!4d-58.834692!3m4!1s0x94456ca42a17f699:0x25bb7a681f9988c!8m2!3d-27.4651173!4d-58.8347331?hl=es">
-                <FaMapMarkedAlt className="ml-2 text-sm text-red-700" />
-              </a>*/ }
-{/* Dorrego 27, B8000FLB Bahía Blanca, Provincia de Buenos Aires
-              <a href="https://www.google.com/search?q=oliva+bahia+blanca&tbm=lcl&sxsrf=ALiCzsZ4e4P7z-lrtLotbUcD9jZlmQ2pXw%3A1657952055404&ei=N1fSYs2fGLmb1sQPq7WtmAY&oq=olivia+bahua&gs_l=psy-ab.3.0.0i13i10k1j0i30i8i13k1l2j0i30i15i8i13i10k1.47519.52019.0.52961.16.14.2.0.0.0.187.1487.9j5.14.0....0...1c.1.64.psy-ab..0.16.1490...35i39k1j0i67k1j0i433i67k1j0i512i433k1j0i512k1j0i512i263i20k1j0i512i10k1j0i10k1j0i203k1j0i30i22k1j0i30i22i10k1.0.15zMhP8IgeA#rlfi=hd:;si:2941287101010715358;mv:[[-38.714393322680976,-62.26114344009053],[-38.71475327731904,-62.26160475990946]]">
-                <FaMapMarkedAlt className="ml-2 text-sm text-red-700" />
-              </a>*/ }
-{/* <span className="text-gray-700 font-bold pr-2">Dirección:</span>{" "}
-              Placido Martínez 1102, W3400 BIJ, Corrientes
-              <a href="https://www.google.com/maps/place/Crist%C3%B3bal+Caf%C3%A9+Corrientes/@-27.4621727,-58.8364824,19z/data=!4m12!1m6!3m5!1s0x94456dd06ab77089:0xd7b4350150605d00!2sPaseo+Wynwood!8m2!3d-27.4622929!4d-58.8361713!3m4!1s0x0:0x125186cf4f713b02!8m2!3d-27.4620456!4d-58.836148">
-                <FaMapMarkedAlt className="ml-2 text-sm text-red-700" />
-              </a>*/ }
