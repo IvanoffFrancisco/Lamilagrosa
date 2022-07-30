46e30c5df1366f0de101b75857e69180f1f92431
MbochaL <martinbosch1996@gmail.com>
2022-07-30 02:57:15 -0300

    se agregan paginas de perfil e historial

---
+++ pages/profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProfileSideBar from '../components/ProfileSideBar'
+
+
+export default function Profile() {
+    return (
+    <div>
+        <ProfileSideBar />
+    </div>
+    )
+}
