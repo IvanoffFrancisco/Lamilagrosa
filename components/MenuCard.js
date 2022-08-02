3410218eb0b2027e6cea1a1fd6a27a467445554f
sebaszwenger <sebaszwenger@gmail.com>
2022-08-02 11:34:45 -0300

    se puso lazy loading

--- components/MenuCard.js
+++ components/MenuCard.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const MenuCard = ({ item,tipo }) => {
+const MenuCard = ({ item, tipo }) => {
   return (
     <li className="h-full text-center bg-black shadow-md shadow-gray-700 hover:ring-2 hover:ring-red-600 rounded-md">
       <div>
@@ -13,12 +13,19 @@ const MenuCard = ({ item,tipo }) => {
           alt="producto"
           objectFit="cover"
           className="rounded-md"
+          loading="lazy"
         />
         <div>
           <h2 className="font-bold text-lg text-white px-1">{item.nombre}</h2>
           <div className="flex justify-evenly items-center py-1 mb-1">
             <p className="text-red-600 text-2xl">$ {item.precio}</p>
-            <Link href={tipo==="menu" ? `/detalleMenu/${item._id}`: `/guarnicion/${item._id}`}>
+            <Link
+              href={
+                tipo === "menu"
+                  ? `/detalleMenu/${item._id}`
+                  : `/guarnicion/${item._id}`
+              }
+            >
               <a className="py-[6px] px-3 bg-red-600 text-white text-sm rounded-md">
                 Añadir
               </a>
